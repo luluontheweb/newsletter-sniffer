ff0d90afefade22692f185472f90ae83be7654dc
luluontheweb <96322105+luluontheweb@users.noreply.github.com>
2026-08-06 02:41:46 -0300

    1.1.0 - added progress checking - reworked discord handling - other small changes

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import { readFileSync, writeFileSync, existsSync, appendFileSync } from "fs";
 import * as cheerio from "cheerio";
 import axios from "axios";
 const newslettersURL = "https://toby.fangamer.com";
+const progressURL = "https://deltarune.com/7b/";
 
-const newsletters = existsSync("newsletters.json") ? JSON.parse(readFileSync("newsletters.json", "utf8")) : [];
-const saveNewsletters = () => writeFileSync("newsletters.json", JSON.stringify(newsletters, null, 2));
+// essentials
+const state = existsSync("state.json") ? JSON.parse(readFileSync("state.json", "utf8")) : { newsletters: [], progress: "" };
+const saveState = () => writeFileSync("state.json", JSON.stringify(state, null, 2));
 const log = async (data, error) => {
     const timestamp = new Date().toISOString();
     if (error) {
@@ -17,36 +19,80 @@ const log = async (data, error) => {
     };
 };
 const sleep = (s) => new Promise(resolve => setTimeout(resolve, s * 1000));
-const post = async (title, description, url) => {
-    try {
-        await axios.post(process.env.webhook, { content: "@everyone", embeds: [{ author: { name: "New newsletter!" }, color: 0xffff00, title: title, description: description, url: url }] });
-    } catch (e) {
-        if (e.status !== 429) throw e;
-        await sleep(e.data?.["retry_after"] ?? 10);
-        return post(title, description, url);
-    };
+
+// discord
+const discordQueue = [];
+let discordHandler = null;
+const postDiscord = (name, color, title, description, url) => {
+    discordQueue.push({ content: "@everyone", embeds: [{ author: { name }, color, title, description, url }] });
+    if (!discordHandler) discordHandler = (async () => {
+        const post = async (content) => {
+            try {
+                await axios.post(process.env.webhook, content);
+            } catch (e) {
+                if (e.status !== 429) throw e;
+                await sleep(e.data?.["retry_after"] ?? 10);
+                return post(content);
+            };
+        };
+        while (discordQueue.length > 0) {
+            log(`Discord queue size: ${discordQueue.length}`);
+            const content = discordQueue.shift();
+            await post(content);
+        };
+        log(`Discord queue empty!`);
+        discordHandler = null;
+    })();
 };
-const check = async () => {
+
+// crawlers
+const checkNewsletters = async () => {
     log("Checking for new newsletters...");
     try {
         const newslettersPage = await axios.get(`${newslettersURL}/newsletters`);
         const $ = cheerio.load(newslettersPage.data);
-        $("#articles").children().each(async (_, article) => {
+        $("#articles").children().get().reverse().forEach((article) => {
             const href = article.attribs['href'];
-            if (!newsletters.includes(href)) {
-                newsletters.push(href);
-                saveNewsletters();
+            if (!state.newsletters.includes(href)) {
+                state.newsletters.push(href);
+                saveState();
                 const url = `${newslettersURL}${href}`;
                 const [title, description] = $(article).text().split('\n').map(s => s.trim()).filter(s => s);
                 log(`NEW NEWSLETTER! ${url}\n    ${title}\n    ${description}`);
-                post(title, description, url);
+                postDiscord("New newsletter!", 0xffff00, title, description, url);
             };
         });
     } catch (e) {
         log("error", e);
     };
+};
+
+const checkProgress = async () => {
+    log("Checking for progress...");
+    try {
+        const progressPage = await axios.get(progressURL);
+        const $ = cheerio.load(progressPage.data);
+        const innerText = $('body').prop('innerText');
+        if (innerText !== state.progress) {
+            state.progress = innerText;
+            saveState();
+            log(`NEW PROGRESS!\n    ${innerText}`);
+            postDiscord(undefined, 0x00ff00, "New progress!", `\`\`\`\n${innerText}\n\`\`\``, progressURL);
+        };
+    } catch (e) {
+        log("error", e);
+    };
+};
+
+// initializer
+const check = async () => {
+    await checkNewsletters();
+    await checkProgress();
     log("Check complete.");
     setTimeout(check, 60000);
 };
-log("Hello World!");
-check();
+
+(() => {
+    log("Hello World!");
+    check();
+})();
